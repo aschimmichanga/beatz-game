700dd0a306f93a351716beaf51165d20983e0300
aschimmichanga <aschimmichanga@users.noreply.github.com>
2019-06-12 11:03:32 -0400

    inserted code for note and song classes.. still needs troubleshooting

---
+++ core/src/com/mygdx/game/ScoreStreaks.java
@@ -0,0 +1,23 @@
+package com.mygdx.game;
+
+public class Scorestreaks {
+
+    private int timeElapsed;
+    private int scoreValue = 0;
+    private int perfect = 0;
+    private int good = 0;
+    private int miss = 0;
+
+    public Scorestreaks()
+    {
+        timeElapsed = timeArrived - timeHit;
+
+        if(timeElapsed <= 0.2500)
+        {
+            perfect++;
+            System.out.println("perfect!");
+            scoreValue += 10;
+        }
+    }
+
+}
--- core/src/com/mygdx/game/Song.java
+++ core/src/com/mygdx/game/Song.java
@@ -1,4 +1,25 @@
 package com.mygdx.game;
+
+import java.util.List;
+
 //OLIVIA/ANUHYA
-public class Song {
+public class Song
+{
+
+    private int offset;
+    private songPosition;
+    private int t = Song.songPosition;//at end
+    private int bpm = t/60;
+    private int beatLength =  60/bpm;
+    List<Note> beatmap = new List<Note>();
+
+    public Song()
+    {
+        offset = .2f;
+        songPosition = (float)(AudioSettings.dspTime – dsptimesong) * song.pitch – offset;
+    }
+
 }
+
+    //Offset: Start time - song start;
+
