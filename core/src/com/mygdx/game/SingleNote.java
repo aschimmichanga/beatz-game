242f22a8820671d93cd514c29688200fab424252
oliviathiel <51379929+oliviathiel@users.noreply.github.com>
2019-06-12 10:27:14 -0400

    Update SingleNote.java

--- core/src/com/mygdx/game/SingleNote.java
+++ core/src/com/mygdx/game/SingleNote.java
@@ -1,19 +1,15 @@
-public abstract class Note
+public class SingleNote extends Note
 {
-  private int noteNum; //1-4
-  private double timeInSong; //what song position the note occurs at
-
-  public Note(int n, int t)
-  {
-      noteNum = n;
-      timeInSong = t;
-  }
-  private double getTime()
-  {
-    return timeInSong;
-  }
-  private int getNoteNum()
-  {
-    return noteNum;
-  }
+    public SingleNote(int n, int t)
+    {
+        super(n, t);
+    }
+    private double getTime()
+    {
+        super.getTime();
+    }
+    private int getNoteNum()
+    {
+        super.getNoteNum();
+    }
 }
