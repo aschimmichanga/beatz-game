0d7275d855436cdc30704fd934e78f42db316d39
oliviathiel <51379929+oliviathiel@users.noreply.github.com>
2019-06-12 12:40:44 -0400

    Update Song.java

--- core/src/com/mygdx/game/Song.java
+++ core/src/com/mygdx/game/Song.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import java.util.List;
+import java.net.URL;
+import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
 //OLIVIA/ANUHYA
 public class Song
@@ -13,20 +16,34 @@ public class Song
     private int t = Song.songPosition;//at end --- what is this for exactly?
     private int bpm;
     private double beatLength =  60/bpm;
-    private String songName; // "xxx.mp3"
+    private String fileName; // "xxx.mp3"
     List<Note> beatmap = new List<Note>(); 
 
-    public Song(String s, double o, int b)
+    public Song(String f, double o, int b)
     {
         offset = o;
-        songName = s;
+        fileName = s;
         bpm = b;
         songPosition = (float)(AudioSettings.dspTime – dsptimesong) * bpm – offset;
     }
     
-   Media hit = new Media(new File(songName).toURI().toString());
-   MediaPlayer mediaPlayer = new MediaPlayer(hit);
-   mediaPlayer.play();
+public class VLC extends Application 
+{
+    private void playMedia() 
+    {
+        URL resource = getClass().getResource(fileName);
+        System.out.println(resource.toString());
+        Media media = new Media(resource.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+    public static void main(String args[]) {
+        new VLC().playMedia();
+    }
+    @Override
+    public void start(Stage stage) throws Exception 
+    {
+    }
 }
 
     //Offset: Start time - song start;
