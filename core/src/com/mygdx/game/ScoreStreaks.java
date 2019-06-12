c8a01f52309d22d639bd2cf6cff441b0118eb17a
anuhyasarang <anuhyasarang@gmail.com>
2019-06-12 15:04:43 -0400

    wrote the score streaks class; not complete, needs to be tested.

--- core/src/com/mygdx/game/ScoreStreaks.java
+++ core/src/com/mygdx/game/ScoreStreaks.java
@@ -12,12 +12,32 @@ public class Scorestreaks {
     {
         timeElapsed = timeArrived - timeHit;
 
-        if(timeElapsed <= 0.2500)
+        if(timeElapsed <= 0.100)
         {
             perfect++;
             System.out.println("perfect!");
             scoreValue += 10;
         }
+        else if(timeElapsed <=0.250)
+        {
+            good++;
+            System.out.println("Good");
+            scoreValue += 10;
+         }
+        else if (timeElapsed > .250)
+        {
+            miss++;
+            System.out.println("Miss. You weeb :(");
+        }
+
+        //score-streaks
+        if (perfect == 5 )//change, so that the score multiplies on correct intervals.
+            scorevalue *=2;
+        else if(perect == 10)
+            scorevalue *= 5;
+        else if (perfect == 15)
+            scoreValue *= 10;
+    }
     }
 
 }
