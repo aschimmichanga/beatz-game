8f6703a06aa3e4d146fe667a3a86396f0e97e639
oliviathiel <51379929+oliviathiel@users.noreply.github.com>
2019-06-12 12:27:14 -0400

    Update Song.java

--- core/src/com/mygdx/game/Song.java
+++ core/src/com/mygdx/game/Song.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import java.util.List;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 //OLIVIA/ANUHYA
 public class Song
@@ -11,18 +13,20 @@ public class Song
     private int t = Song.songPosition;//at end --- what is this for exactly?
     private int bpm;
     private double beatLength =  60/bpm;
-    private String songName;
+    private String songName; // "xxx.mp3"
     List<Note> beatmap = new List<Note>(); 
 
     public Song(String s, double o, int b)
     {
         offset = o;
-        songname = s;
+        songName = s;
         bpm = b;
         songPosition = (float)(AudioSettings.dspTime – dsptimesong) * bpm – offset;
-        
     }
-
+    
+   Media hit = new Media(new File(songName).toURI().toString());
+   MediaPlayer mediaPlayer = new MediaPlayer(hit);
+   mediaPlayer.play();
 }
 
     //Offset: Start time - song start;
