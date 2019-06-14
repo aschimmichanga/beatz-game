12ec283997a1055b51c4b7facaab1b36f1a0c93d
aschimmichanga <ashnasrivastava1@gmail.com>
2019-06-14 14:13:24 -0400

    implemented Audio playing

--- core/src/com/mygdx/game/Audio.java
+++ core/src/com/mygdx/game/Audio.java
@@ -1,9 +1,50 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 
-public class Audio
+
+
+public class Audio implements ApplicationListener
 {
-    music = Gdx.audio.newMusic(Gdx.files.internal("Electro_-_Noisestorm_-_Surge_Original_Mix_Monstercat_EP_Release.mp3");
-    music.play();
+    private Sound music;
+    public void create()
+    {
+        music = Gdx.audio.newSound(Gdx.files.internal("ElectroSurge.mp3"));
+        music.play();
+    }
+
+    @Override
+    public void dispose()
+    {
+        music.dispose();
+    }
+
+    private void update(){}
+
+    @Override
+    public void render()
+    {
+        music.play(0.5f);
+
+
+        update();
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.*;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Audio(), config);
 	}
 }
+
+
+
+
+
