19001ea323f86698c424e505c53b3a81bbba71ab
oliviathiel <oliviasthiel72@gmail.com>
2019-06-18 13:33:05 -0400

    created tester

--- core/src/com/mygdx/game/Song.java
+++ core/src/com/mygdx/game/Song.java
@@ -8,18 +8,18 @@ public class Song
     private String fileName; // "xxx.mp3"
     private double offset;
     private int bpm;
-    private double time;
     private double songPosition;
     private double beatLength;
     private ArrayList<Note> beatmap;
+    private Music music;
 
     public Song(String file, double o, int b)
     {
         fileName = file;
         offset = o;
         bpm = b;
-        time = songPosition;//at end --- what is this for exactly?
-        //songPosition = ((float)(AudioSettings.dspTime) – dsptimesong) * bpm – offset;
+        songPosition = ((float)(music.getPosition() / 60.0) * bpm – offset); //calcs what beat it's at out of total beats,
+        // assuming pos is in seconds
         beatLength =  60.0 / bpm;
         beatmap = new ArrayList<Note>();
 
@@ -28,6 +28,10 @@ public class Song
     {
         return fileName;
     }
+
+    private double getSongPosition() { return songPosition; }
+
+    private double getBeatLength() { return beatLength; }
     //finish writing the setters and getters
 }
 
